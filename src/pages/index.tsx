9e4c3425558c80149f6d6ca837fffad9916d8f16
shoaibansari007 <141940803+shoaibansari007@users.noreply.github.com>
2024-05-12 12:34:16 +0530

    fix : buttons position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -150,21 +150,23 @@ export default function Home() {
             className="picture absolute bottom-44 md:bottom-12  max-sm:transform max-sm:translate-x-[50%] right-[50%]  lg:bottom-0 md:right-20 lg:right-60 w-72"
           />
           <div className="absolute bottom-20 lg:bottom-0   w-full p-8 pb-0 md:pb-12 md:p-12 md:bottom-0">
-            <h3 className="text-4xl mb-7 font-bold">Fortune Fox</h3>
-            <h3 className="text-4xl mb-7 font-bold">Now on Solana!</h3>
+            <h3 className="text-4xl mb-7 font-bold mx-auto">Fortune Fox</h3>
+            <h3 className="text-4xl mb-7 font-bold mx-auto">Now on Solana!</h3>
             <div className="w-full  flex flex-col md:flex-row gap-4">
               <a
                 href="https://jup.ag/swap/SOL-BOJACK_EUeJuDgVxx83953XeBHS3fzY7tneitb19dmdPrTMHGqd"
                 target="_blank"
-                className="flex hover:bg-white/80 hover:text-black rounded-3xl p-3 px-6 text-2xl bg-white text-black min-w-[260px] gap-3 max-w-fit"
+                className="flex hover:bg-white/80 hover:text-black rounded-3xl p-3 px-6 text-2xl bg-white text-black min-w-full text-center md:min-w-[260px] gap-3 max-w-fit"
               >
-                Buy on Jupiter
-                <img src="assets/explore.svg" alt="" />
+                <div className="flex gap-3 mx-auto">
+                  Buy on Jupiter
+                  <img src="assets/explore.svg" alt="" />
+                </div>
               </a>
               <a
                 href="https://twitter.com/BojackSolana"
                 target="_blank"
-                className="hover:bg-white/80 hover:text-black rounded-3xl p-3 px-6 text-2xl bg-white text-black min-w-[260px] max-w-fit"
+                className="hover:bg-white/80 hover:text-black rounded-3xl p-3 px-6 text-2xl bg-white text-black min-w-full text-center md:min-w-[260px] max-w-fit"
               >
                 Follow on Twitter
               </a>
